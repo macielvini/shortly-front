f9e89d413a9d19f730042dc9e3e379e725a51c8a
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-12-16 15:25:21 -0300

    chore: prepare code for deploy

---
+++ src/assets/css/userStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const UserStyles = createGlobalStyle`
+  body {
+    font-family: 'Lexend Deca', sans-serif;
+    background-color: lightblue;
+
+  }
+`;
+
+export default UserStyles;
